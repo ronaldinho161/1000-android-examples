7fe3731da595916d952b94b6960128f3ad7e0105
Anton Antonov <ajantonov@gmail.com>
2017-01-26 12:28:33 +0200

    Implemented TwitterSearches2 app.

---
+++ TwitterSearches2/app/src/main/java/eu/aplusteam/twittersearches/ItemDivider.java
@@ -0,0 +1,41 @@
+package eu.aplusteam.twittersearches;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by anton on 26.01.17.
+ * Divider for RecyclerView
+ */
+
+class ItemDivider extends RecyclerView.ItemDecoration {
+    private final Drawable divider;
+
+    ItemDivider(Context context) {
+        int[] attrs = {android.R.attr.listDivider};
+        divider = context.obtainStyledAttributes(attrs).getDrawable(0);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDrawOver(c, parent, state);
+
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        for (int i = 0; i < parent.getChildCount() - 1; ++i) {
+            View item = parent.getChildAt(i);
+
+            int top = item.getBottom() +
+                    ((RecyclerView.LayoutParams)
+                            item.getLayoutParams()).bottomMargin;
+            int bottom = top + divider.getIntrinsicHeight();
+
+            divider.setBounds(left, top, right, bottom);
+            divider.draw(c);
+        }
+    }
+}
